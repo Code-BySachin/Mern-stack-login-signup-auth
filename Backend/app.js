7d6e24ac3f50f8930eaad9821b685441159de852
Code-BySachin <sachin680125@gmail.com>
2025-08-18 14:16:57 +0530

    feat: add basic server

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('API Working')
+})
+
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`);
+    
+})
