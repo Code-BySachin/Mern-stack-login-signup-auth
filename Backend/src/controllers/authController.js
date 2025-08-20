2446b7226f821c6841ddc0476d5f2571f686267b
Code-BySachin <sachin680125@gmail.com>
2025-08-20 17:37:54 +0530

    feat: add login controller logic

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 
 export const register = async (req,res)=>{
-    const {name,email,password} =req.body;
+    const {name,email,password} =req.body || {};
 
     if(!name || !email || !password){
         return res.json({success:false, message:'All fields are mandatory'})
@@ -35,4 +35,42 @@ export const register = async (req,res)=>{
     }catch(err){
         return res.json({success:false,message:err.message})
     }
+}
+
+export const login = async (req,res)=>{
+    const {email,password} = req.body || {};
+
+    if(!email || !password){
+        return res.json({success:false, message:'All fields are mandatory !'})
+    }
+    try{
+
+        const user = await userModel.findOne({email:email});
+        
+        if(!user){
+            return res.json({success:false, message: 'Invalid email'});
+        }
+        
+        const isMatch = await bcrypt.compare(password,user.password);
+        console.log('ismatch: ',isMatch);
+        
+        
+        if(!isMatch){
+            return res.json({success:false, message:'Invalid password'});
+        }
+        const token = jwt.sign({user:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+        res.cookie('token',token,{
+            httpOnly:true,
+            sameSite:process.env.NODE_ENV === 'production',
+            secure:process.env.NODE_ENV === 'production',
+            maxAge: 7*24*60*60*1000
+        })
+
+        return res.json({success:true,message:'User logged in'})
+
+    }catch(err){
+        return res.json({success:false, message:err.message});
+    }
+
+    
 }
