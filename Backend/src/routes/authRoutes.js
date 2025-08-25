ef1ede221f36cd525d7603998fd8a627c8c41bd9
Code-BySachin <sachin680125@gmail.com>
2025-08-25 08:33:46 +0530

    feat: send reset otp

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, register, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import { login, logout, register, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
 
 
 const authRouter = express.Router();
@@ -9,6 +9,6 @@ authRouter.post('/login',login);
 authRouter.post('/logout',logout);
 authRouter.post('/sendOtp',sendVerifyOtp);
 authRouter.post('/verifyemail',verifyEmail);
-
+authRouter.post('/send-reset-otp',sendResetOtp)
 
 export default authRouter;
